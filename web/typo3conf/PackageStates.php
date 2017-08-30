caafa223f01ec7cdc3e63b2e8edf8b32a9fcde71
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2017-08-30 09:37:01 +0200

    Add PackageStates and add Development Context for docker

---
+++ web/typo3conf/PackageStates.php
@@ -0,0 +1,115 @@
+<?php
+# PackageStates.php
+
+# This file is maintained by TYPO3's package management. Although you can edit it
+# manually, you should rather use the extension manager for maintaining packages.
+# This file will be regenerated automatically if it doesn't exist. Deleting this file
+# should, however, never become necessary if you use the package commands.
+
+return [
+    'packages' => [
+        'core' => [
+            'packagePath' => 'typo3/sysext/core/',
+        ],
+        'extbase' => [
+            'packagePath' => 'typo3/sysext/extbase/',
+        ],
+        'fluid' => [
+            'packagePath' => 'typo3/sysext/fluid/',
+        ],
+        'frontend' => [
+            'packagePath' => 'typo3/sysext/frontend/',
+        ],
+        'fluid_styled_content' => [
+            'packagePath' => 'typo3/sysext/fluid_styled_content/',
+        ],
+        'info' => [
+            'packagePath' => 'typo3/sysext/info/',
+        ],
+        'info_pagetsconfig' => [
+            'packagePath' => 'typo3/sysext/info_pagetsconfig/',
+        ],
+        'extensionmanager' => [
+            'packagePath' => 'typo3/sysext/extensionmanager/',
+        ],
+        'lang' => [
+            'packagePath' => 'typo3/sysext/lang/',
+        ],
+        'version' => [
+            'packagePath' => 'typo3/sysext/version/',
+        ],
+        'workspaces' => [
+            'packagePath' => 'typo3/sysext/workspaces/',
+        ],
+        'backend' => [
+            'packagePath' => 'typo3/sysext/backend/',
+        ],
+        'belog' => [
+            'packagePath' => 'typo3/sysext/belog/',
+        ],
+        'beuser' => [
+            'packagePath' => 'typo3/sysext/beuser/',
+        ],
+        'cshmanual' => [
+            'packagePath' => 'typo3/sysext/cshmanual/',
+        ],
+        'documentation' => [
+            'packagePath' => 'typo3/sysext/documentation/',
+        ],
+        'filelist' => [
+            'packagePath' => 'typo3/sysext/filelist/',
+        ],
+        'filemetadata' => [
+            'packagePath' => 'typo3/sysext/filemetadata/',
+        ],
+        'install' => [
+            'packagePath' => 'typo3/sysext/install/',
+        ],
+        'recordlist' => [
+            'packagePath' => 'typo3/sysext/recordlist/',
+        ],
+        'recycler' => [
+            'packagePath' => 'typo3/sysext/recycler/',
+        ],
+        'rte_ckeditor' => [
+            'packagePath' => 'typo3/sysext/rte_ckeditor/',
+        ],
+        'saltedpasswords' => [
+            'packagePath' => 'typo3/sysext/saltedpasswords/',
+        ],
+        'scheduler' => [
+            'packagePath' => 'typo3/sysext/scheduler/',
+        ],
+        'sv' => [
+            'packagePath' => 'typo3/sysext/sv/',
+        ],
+        't3editor' => [
+            'packagePath' => 'typo3/sysext/t3editor/',
+        ],
+        'tstemplate' => [
+            'packagePath' => 'typo3/sysext/tstemplate/',
+        ],
+        'find' => [
+            'packagePath' => 'typo3conf/ext/find/',
+        ],
+        'news' => [
+            'packagePath' => 'typo3conf/ext/news/',
+        ],
+        'pazpar2' => [
+            'packagePath' => 'typo3conf/ext/pazpar2/',
+        ],
+        'realurl' => [
+            'packagePath' => 'typo3conf/ext/realurl/',
+        ],
+        'solr' => [
+            'packagePath' => 'typo3conf/ext/solr/',
+        ],
+        'tmpl_fidaac' => [
+            'packagePath' => 'typo3conf/ext/tmpl_fidaac/',
+        ],
+        'vhs' => [
+            'packagePath' => 'typo3conf/ext/vhs/',
+        ],
+    ],
+    'version' => 5,
+];
